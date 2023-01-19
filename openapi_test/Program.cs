4f21b4ef2f07805c141361f72b8b5bd565ca7b4a
thomaszimny-prologis <63655595+thomaszimny-prologis@users.noreply.github.com>
2023-01-19 12:56:38 +0100

    init

---
+++ openapi_test/Program.cs
@@ -0,0 +1,24 @@
+﻿using openapi_test.SendITApi;
+
+var httpClient = new HttpClient();
+var apiClient = new SendITApiClient(httpClient)
+{
+    BaseUrl = "http://s21514564.onlinehome-server.info:4210/"
+};
+
+try
+{
+	var cancelResponse = await apiClient.PackageV2_CancelAsync(new CancelRequest
+	{
+		ClientID = "0001",
+		PackageID = "P1",
+		WebServiceUser = "testUser",
+		WebServicePassword = "testPassword"
+	});
+
+    Console.WriteLine($"Cancel Successful: Result: {cancelResponse.Result}");
+}
+catch (ApiException ex)
+{
+    Console.WriteLine($"Result: HTTP status {ex.StatusCode} Message: {ex.Response}");
+}
